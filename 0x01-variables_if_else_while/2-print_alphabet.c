ab0f277881bd30c0d1249832e5523953db2fe444
bewketu21 <abewketu@gmail.com>
2022-12-09 04:54:44 -0800

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: print the english alphabet in order
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+}

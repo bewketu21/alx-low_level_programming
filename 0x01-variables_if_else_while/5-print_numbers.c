ed61195ac0cc793dd377466bbcc1f89bac643a77
bewketu21 <abewketu@gmail.com>
2022-12-09 05:40:29 -0800

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Prints all single digit numbers of base 10 starting from 0
+ * Return: 0
+ */
+int main(void)
+{
+	int c = '0';
+
+	while (c < 10)
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}

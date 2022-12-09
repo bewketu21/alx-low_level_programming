eb56236c178482c64a9baa2bc9579d63b544909a
bewketu21 <abewketu@gmail.com>
2022-12-09 06:53:47 -0800

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,11 +9,11 @@ int main(void)
 	int first_nums;
 	int second_nums;
 
-	for (first_nums = 0; firt_nums < 100; firt_nums++)
+	for (first_nums = 0; first_nums < 100; first_nums++)
 	{
 		for (second_nums = first_nums + 1; second_nums < 100; second_nums++)
 		{
-			putchar(firt_nums / 10 + '0');
+			putchar(first_nums / 10 + '0');
 			putchar(first_nums % 10 + '0');
 
 			putchar(' ');
@@ -21,7 +21,7 @@ int main(void)
 			putchar(second_nums / 10 + '0');
 			putchar(second_nums % 10 + '0');
 
-			if (firt_nums == 98 && second_nums == 99)
+			if (first_nums == 98 && second_nums == 99)
 			{
 				break;
 			}

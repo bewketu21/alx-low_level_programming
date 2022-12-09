59e65582bd471a62bb57fa9baa04a7f3c610b42e
bewketu21 <abewketu@gmail.com>
2022-12-09 05:53:45 -0800

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: prints sigle digit numbers\
+ * Return: 0
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		putchar('c');
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}

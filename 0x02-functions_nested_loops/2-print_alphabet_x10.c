686da8080b68c004ce4333198421ea8c899b1bc2
bewketu21 <abewketu@gmail.com>
2022-12-12 23:02:07 -0800

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+}
+/**
+ * main - main block
+ * Return: Always 0
+ */
+int main(void)
+{
+	print_alphabet_x10();
+	print_alphabet_x10();
+	print_alphabet_x10();
+	print_alphabet_x10();
+	print_alphabet_x10();
+	print_alphabet_x10();
+	print_alphabet_x10();
+	print_alphabet_x10();
+	print_alphabet_x10();
+	print_alphabet_x10();
+	putchar('\n');
+
+	return (0);
+}

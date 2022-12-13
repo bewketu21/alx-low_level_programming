48f2816810b66c029804e9944476840a4c0d0697
bewketu21 <abewketu@gmail.com>
2022-12-12 22:19:14 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * print_alphabet - prints the alphabet
+ * Description - prit all the alpahbet letters in lowercase
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+}
+
+int main(void)
+{
+	print_alphabet();
+	putchar('\n');
+
+	return (0);
+}

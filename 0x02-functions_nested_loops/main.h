0f322f236417f02547c8569dce1f64f41ce823cd
bewketu21 <abewketu@gmail.com>
2022-12-13 05:49:37 -0800

    Task 10

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,4 @@
+#ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
 int main(void);
@@ -13,3 +14,4 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+#endif

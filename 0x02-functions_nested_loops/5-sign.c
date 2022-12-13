eb917725724f1cf8bd90fed045496fe5cb16002d
bewketu21 <abewketu@gmail.com>
2022-12-13 04:00:38 -0800

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * print_sign - prints the sign of the number
  * @n: an integer
@@ -8,48 +8,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar('+');
+		_putchar('+');
 		return (1);
 	}
 	else if (n < 0)
 	{
-		putchar ('-');
+		_putchar ('-');
 		return (-1);
 	}
 	else
 	{
-		putchar('0');
+		_putchar('0');
 		return (0);
 	}
-}
-/**
- * main - main block
- * Return: 0
- */
-int main(void)
-{
-	int r;
-
-	r = print_sign(98);
-	putchar(',');
-	putchar(' ');
-	putchar(r + '0');
-	putchar('\n');
-	r = print_sign(0);
-	putchar(',');
-	putchar(' ');
-	putchar(r + '0');
-	putchar('\n');
-	r = print_sign(0xff);
-	putchar(',');
-	putchar(' ');
-	putchar(r + '0');
-	putchar('\n');
-	r = print_sign(-1);
-	putchar(',');
-	putchar(' ');
-	putchar(r + '0');
-	putchar('\n');
-
-	return (0);
 }

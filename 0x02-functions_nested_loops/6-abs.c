6260fa6948a16833219fed2bbebd2a90e66893ab
bewketu21 <abewketu@gmail.com>
2022-12-13 04:03:26 -0800

    Task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * _abs - computes the absolute value of an integer.
  *@n: an integer number n
@@ -17,23 +17,4 @@ int _abs(int n)
 		abs = n * -1;
 		printf("%d", abs);
 	}
-}
-/**
- * main - main block
- * Return: 0
- */
-int main(void)
-{
-	int r;
-
-	r = _abs(-1);
-	printf("%d\n", r);
-	r = _abs(0);
-	printf("%d\n", r);
-	r = _abs(1);
-	printf("%d\n", r);
-	r = _abs(-98);
-	printf("%d\n", r);
-
-	return (0);
 }

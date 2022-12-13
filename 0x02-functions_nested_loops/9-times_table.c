7993fbd1e5494ac85973f253a355d26031b911f9
bewketu21 <abewketu@gmail.com>
2022-12-13 05:12:16 -0800

    Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * times_table - print 9 times table
+ * Return: 0
+ */
+void times_table(void)
+{
+	int i, j, a;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			a = i * j;
+			printf("%d", a);
+			printf(",\b");
+
+		}
+		printf("\n");
+	}
+}

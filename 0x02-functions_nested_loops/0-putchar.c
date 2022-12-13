78cd64a48fe918d43868a7e23507f07a064ef2f1
bewketu21 <abewketu@gmail.com>
2022-12-13 01:13:49 -0800

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
-#include "main.h"
+#include"main.h"
 /**
- * main - main block
- * Description: print 0-putchar.c
+ * main - main blockDescrip
+ * tion: print 0-putchar.c
  * Return: 0
  */
 int main(void)

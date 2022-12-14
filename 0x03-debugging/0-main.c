8bc15decfa40844b04bf67fa7af98e1b78dde6d7
bewketu21 <abewketu@gmail.com>
2022-12-14 16:37:11 +0300

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,29 +1,13 @@
 #include "main.h"
 /**
- * positive_or_negative - prints
- * @i: an integer i
- * Return: 0
- */
-void positive_or_negative(int i)
-{
-	srand(time(0));
-	i = rand() - RAND_MAX / 2;
-
-	if (i > 0)
-		printf("%d is positive\n", i);
-	else if (i < 0)
-		printf("%d is negative\n", i);
-	else
-		printf("%d is zero\n", i);
-}
-/**
- * main - main block
+ * main - prints positive or negative
  * Return: 0
  */
 int main(void)
 {
 	int i;
 
+	i = 98 % 2;
 	positive_or_negative(i);
 
 	return (0);

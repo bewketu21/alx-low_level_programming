04019204de8cfb966a97d0d5cd8c4e21d8e7c670
bewketu21 <abewketu@gmail.com>
2022-12-14 16:33:06 +0300

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,11 @@
 #include "main.h"
 /**
- * postitive_or_negative - prints
+ * positive_or_negative - prints
  * @i: an integer i
  * Return: 0
  */
-void postitive_or_negative(int i)
+void positive_or_negative(int i)
 {
-	int i;
-
 	srand(time(0));
 	i = rand() - RAND_MAX / 2;
 
@@ -17,8 +15,6 @@ void postitive_or_negative(int i)
 		printf("%d is negative\n", i);
 	else
 		printf("%d is zero\n", i);
-
-	return (0);
 }
 /**
  * main - main block
@@ -28,7 +24,7 @@ int main(void)
 {
 	int i;
 
-	positive_or_negative(int i);
+	positive_or_negative(i);
 
 	return (0);
 }
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,6 +2,9 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 int main(void);
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);

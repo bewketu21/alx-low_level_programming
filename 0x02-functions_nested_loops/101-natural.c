2d2e14273087432d926977dba210cf07648dec41
bewketu21 <abewketu@gmail.com>
2022-12-14 14:57:53 +0300

    Task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - main block
+ * Description: finds the sum of multiples of 3 or 5
+ * Return: 0
+ */
+int main(void)
+{
+	int Tot, n;
+
+	for (n =0; n < 1024; n++)
+	{
+		if ((n % 3 == 0) || (n % 5 == 0))
+		{
+			Tot = Tot + n;
+		}
+	}
+	printf("%d\n", Tot);
+
+	return(0);
+}

a5659b55feeceda18c235979de960aae38cf39a4
bewketu21 <abewketu@gmail.com>
2022-12-15 08:46:30 +0300

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,31 @@
 #include "main.h"
 /**
+ * positive_or_negative - prints whether the number is positive 
+ * or negative
+ * Return: 0
+ */
+void positive_or_negative(int i)
+{
+	int n;
+
+	srand(time(0));
+	n = i;
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else
+	{
+		printf("%d is zero\n", n);
+	}
+	return (0);
+}
+/**
  * main - main block
  * Return: 0
  */

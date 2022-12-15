14a2a0142caef2f3aff03ce35085e99f10307585
bewketu21 <abewketu@gmail.com>
2022-12-15 16:14:54 +0300

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,1 +1,37 @@
-10-print_triangle.c
+#include "main.h"
+/**
+ * main - prints triangle
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 3 == 0 && a % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (a % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if( a % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", a);
+		}
+
+		if (a != 100)
+		{
+			_putchar(' ');
+		}
+	}
+	_putchar('\n');
+
+	return (0);
+}

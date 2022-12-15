11882f0f331e066293e6dca2633675e26d6a778d
bewketu21 <abewketu@gmail.com>
2022-12-15 16:26:09 +0300

    Task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_triangle - prints triangle
+ * @size: triangle size
+ * Return: 0
+ */
+void print_triangle(int size);
+{
+	int b, h;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (h = 1; h <= size; h++)
+		{
+			for (b = 1; b <= size; b++)
+			{
+				if ((h + b) <= size)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}

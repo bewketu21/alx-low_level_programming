9277f8b5a309e60aa90aec52570da1fe1ba93192
bewketu21 <abewketu@gmail.com>
2022-12-19 15:55:24 +0300

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse order
+ * @s: a pointer to an int
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int a;
+
+	a = 0;
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+
+	for (a = a - 1; a >= 0; a--)
+	{
+		_putchar(s[a]);
+	}
+	_putchar('\n\);
+}

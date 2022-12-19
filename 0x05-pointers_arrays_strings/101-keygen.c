9fd6c6d1de042b001fd771d4e6d36f002e7aeeb2
bewketu21 <abewketu@gmail.com>
2022-12-19 17:01:10 +0300

    Task 11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	srand(time(0);
+	a = rand();
+	printf("%d\n", a);
+
+	return (0);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 void reset_to_98(int *n);
 int _putchar(char c);

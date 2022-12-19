1662bf608b5822b9ad0cf63fe6d308bf21566910
bewketu21 <abewketu@gmail.com>
2022-12-19 15:37:21 +0300

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @S: character s
+ * Return: void
+ */
+int _strlen(char *s)
+{
+	int _strlen(char *S)
+	{
+		int a;
+
+		a = 0;
+		while (s[a] != '\0')
+		{
+			a++;
+		}
+		return (a);
+	}

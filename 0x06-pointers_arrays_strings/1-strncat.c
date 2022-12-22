693e73b4f7b1c4b3e6f35f9dfd05ba057c5be287
bewketu21 <abewketu@gmail.com>
2022-12-22 04:28:45 -0800

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,20 +10,16 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int a = 0, b;
 
-	while (dest[a] != '\0')
+	while (dest[a])
 	{
 		a++;
 	}
-	while (n--)
+
+	for (b = 0; b < n && src[b] != '\0'; b++)
 	{
-		if (!(*dest++ = *src++)
-			{
-				return (src);
-			}
-	/** for (b = 0; b < n && src[b] != '\0'; b++)
-	 * {dest[a + b] = src[b];}
-	 */
-	
-	*dest = '\0';
+		dest[a + b] = src[b];
+	}
+	dest[a + b] = '\0';
+
 	return (dest);
 }

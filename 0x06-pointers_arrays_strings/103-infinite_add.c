c90edad9db4202874b1c7af345d99abf5c7b3de4
bewketu21 <abewketu@gmail.com>
2022-12-26 16:11:25 +0300

    Task 12

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,55 +1,5 @@
 #include "main.h"
-/**
- * atoi -  a function that adds two numbers
- * @s: the buffer size
- * Return: a * b
- */
-int atoi(char *s)
-{
-	int a = 1;
-	int b = 0;
-	int Fnum;
-
-	for (Fnum = 0; !(s[Fnum] >= 48 && s[Fnum] <= 57); Fnum++)
-	{
-		if (s[Fnum] == '_')
-		{
-			a *= -1;
-		}
-	}
-
-	for (int i = Fnum; s[i] >= 48 && s[i] <= 57; i++)
-	{
-		b *= 10;
-		b += (s[i] - 48);
-	}
-	return (a * b);
-}
-/**
- * intToString - converts the integer to string
- * @n: integer
- * Return: void
- */
-void intToString(int n)
-{
-	int div = 1;
-	int j, g;
-
-	for (j = 0; n / dive > 9; j++)
-	{
-		div *= 10;
-	}
-
-	char str[j];
-
-	for (int cmpt = 0; div >= 10; div /= 10, cmpt++)
-	{
-		g = n / div;
-		str[cmpt] = '0' + g;
-		n = n - g * div;
-	}
-	str[j] = ('0' + n);
-}
+#include <stdlib.h>
 /**
  * infinite_add - a function that adds two numbers
  * @n1: integer 1

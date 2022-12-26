a57fedb9ef640e5c6e9d506d463abada4db5aa99
bewketu21 <abewketu@gmail.com>
2022-12-25 22:20:04 -0800

    Task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,19 +6,20 @@
  */
 void print_number(int n)
 {
-	if (n / 10 != 0)
+	if (n < 0)
 	{
-		_putchar(n % 10);
-		print_number((n - n % 10) / 10);
+		_putchar('-');
+		n = -1 * n;
 	}
 
-	else if ((n / 10 == 0) && (n % 10 != 0) && (n > 0))
+	if (n == 0)
 	{
-		_putchar(n % 10);
+		_putchar(48);
 	}
-	
-	else if ((n / 10 == 0) && (n % 10 != 0) && (n <= 0))
+
+	if (n / 10)
 	{
-		_putchar(-n % 10);
+		print_number(n / 10);
 	}
+	_putchar(n % 10 + 48);
 }
